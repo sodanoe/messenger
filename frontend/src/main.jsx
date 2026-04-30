028c0e662451a164119fac8df4caea05b5e2fef1
Alexandr Lissovskiy <lisovskiy.a@gmail.com>
2026-04-30 13:21:01 +0300

    add JS hack for Android Chrome

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+const isAndroid = /Android/i.test(navigator.userAgent);
+
+if (isAndroid) {
+  function setVH() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  setVH();
+  window.addEventListener('resize', setVH);
+  window.addEventListener('orientationchange', setVH);
+  if (window.visualViewport) {
+    window.visualViewport.addEventListener('resize', setVH);
+  }
+}
+
 createRoot(document.getElementById('root')).render(<App />);
